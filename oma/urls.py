967e72dbe6268081570c02d89b0c04305d8a75ed
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2014-07-25 16:37:55 +0200

    add hog member view, some improvements on the database querying
    Signed-off-by: Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>

--- oma/urls.py
+++ oma/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 from . import views
 urlpatterns = patterns('',
         url(r'^pairs/(?P<entry_id>\w+)/$', views.pairs, name='index'),
+        url(r'^hogs/(?P<entry_id>\w+)/(?P<level>\w+)/$', views.hogs, name='hogs'),
 )
--- oma/views.py
+++ oma/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from . import utils
 
@@ -16,5 +16,20 @@ def pairs(request, entry_id, idtype='OMA'):
     context = {'query': query, 'vps':vps}
     
     return render(request, 'vpairs.html', context)
+
+def hogs(request, entry_id, level, idtype='OMA'):
+    entry_nr = utils.id_resolver.resolve(entry_id)
+    query = utils.id_mapper[idtype].map_entry_nr(entry_nr)
+    try:
+        hog_members = utils.db.hog_members(entry_nr, level)
+        print(hog_members)
+        hog_member_ids = map(utils.id_mapper[idtype].map_entry_nr, hog_members['EntryNr'])
+    except utils.Singleton:
+        hog_member_ids = [query]
+    except ValueError as e:
+        raise Http404(e.message)
+    context = {'query': query, 'level': level, 'hog_members': hog_member_ids} 
+    return render(request, 'hogs.html', context)
+
 
 
