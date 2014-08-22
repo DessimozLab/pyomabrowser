9281fc062a516034aa8ff0d4cb5bc5b301c6cc94
train <train@public-docking-cx-2077.ethz.ch>
2014-08-22 18:25:15 +0200

    i hate git adrian

--- oma/urls.py
+++ oma/urls.py
@@ -4,5 +4,29 @@ urlpatterns = patterns('',
           url(r'^pairs/(?P<entry_id>\w+)/$', views.pairs, name='index'),
         url(r'^hogs/(?P<entry_id>\w+)/(?P<level>\w+)/$', views.hogs, name='hogs'),
         url(r'^home/$', views.home, name='home'),
+        url(r'^about/$', views.about, name='about'),
+        url(r'^groupCV/$', views.groupCV, name='groupCV'),
+        url(r'^landHOG/$', views.landHOG, name='landHOG'),
+        url(r'^landAnnotation/$', views.landAnnotation, name='landAnnotation'),
+        url(r'^landsynteny/$', views.landsynteny, name='landsynteny'),
+        url(r'^team/$', views.team, name='team'),
+        url(r'^funding/$', views.funding, name='funding'),
+        url(r'^license/$', views.license, name='license'),
+        url(r'^current/$', views.current, name='current'),
+        url(r'^archives/$', views.archives, name='archives'),
+        url(r'^APISOAP/$', views.APISOAP, name='APISOAP'),
+        url(r'^APIDAS/$', views.APIDAS, name='APIDAS'),
+        url(r'^type/$', views.type, name='type'),
+        url(r'^uses/$', views.uses, name='uses'),
+        url(r'^FAQ/$', views.FAQ, name='FAQ'),
+        url(r'^genomePW/$', views.genomePW, name='genomePW'),
+        url(r'^nutshell/$', views.nutshell, name='nutshell'),
+        url(r'^dataset/$', views.dataset, name='dataset'),
+        url(r'^landOMA/$', views.landOMA, name='landOMA'),
+
+
+
+
+
 #        url(r'^synteny/(?P<entry_id>\w+)/(?P<windows>\w+)/$', views.synteny, name='synteny'),
 )
--- oma/views.py
+++ oma/views.py
@@ -35,4 +35,83 @@ def home(request):
     context = {}
     return render(request, 'home.html', context)
 
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
+
+def groupCV(request):
+    context = {}
+    return render(request, 'groupCV.html', context)
+
+def landHOG(request):
+    context = {}
+    return render(request, 'landHOG.html', context)
+
+def landAnnotation(request):
+    context = {}
+    return render(request, 'landAnnotation.html', context)
+
+
+def landsynteny(request):
+    context = {}
+    return render(request, 'landsynteny.html', context)
+
+def team(request):
+    context = {}
+    return render(request, 'team.html', context)
+
+def funding(request):
+    context = {}
+    return render(request, 'funding.html', context)
+
+def license(request):
+    context = {}
+    return render(request, 'license.html', context)
+
+def current(request):
+    context = {}
+    return render(request, 'current.html', context)
+
+def archives(request):
+    context = {}
+    return render(request, 'archives.html', context)
+
+def APISOAP(request):
+    context = {}
+    return render(request, 'APISOAP.html', context)
+
+def APIDAS(request):
+    context = {}
+    return render(request, 'APIDAS.html', context)
+
+def type(request):
+    context = {}
+    return render(request, 'type.html', context)
+
+def uses(request):
+    context = {}
+    return render(request, 'uses.html', context)
+
+def FAQ(request):
+    context = {}
+    return render(request, 'FAQ.html', context)
+
+def genomePW(request):
+    context = {}
+    return render(request, 'genomePW.html', context)
+
+def nutshell(request):
+    context = {}
+    return render(request, 'nutshell.html', context)
+
+def dataset(request):
+    context = {}
+    return render(request, 'dataset.html', context)
+
+
+def landOMA(request):
+    context = {}
+    return render(request, 'landOMA.html', context)
+
+
 
