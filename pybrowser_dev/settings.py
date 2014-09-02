dd3b8795960ea34edf392754a761372ab949bcc4
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2014-09-02 15:24:28 +0200

    set static root for deployment

--- pybrowser_dev/settings.py
+++ pybrowser_dev/settings.py
@@ -86,5 +86,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = os.path.join(os.path.expanduser("~"),"Browser","htdocs","static")
 STATIC_URL = '/static/'
