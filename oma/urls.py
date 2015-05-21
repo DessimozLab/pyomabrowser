1a9330256f5ebbe661581f2f842b47c65d411f15
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2015-05-21 15:15:52 +0200

    fixed a few typos in fellowship pages

--- oma/urls.py
+++ oma/urls.py
@@ -32,7 +32,7 @@ urlpatterns = patterns('',
         url(r'^FAQ/$', TemplateView.as_view(template_name='FAQ.html'), name='FAQ'),
         url(r'^genomePW/$', TemplateView.as_view(template_name='genomePW.html'), name='genomePW'),
         url(r'^landOMA/$', TemplateView.as_view(template_name='landOMA.html'), name='landOMA'),
-        url(r'^fellowship/?$', views.fellowship, name="fellowship"),
+        url(r'^fellowship/$', views.fellowship, name="fellowship"),
         url(r'^thanks/', TemplateView.as_view(template_name='thanks.html'), name='thanks'),
 
         url(r'^current/$', views.CurrentView.as_view(), name='current'),
