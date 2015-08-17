ce3ceaa81d7b4620c5f5cc713d11ac98587978e1
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2015-08-17 11:43:44 +0200

    DEPLOYMENT_TYPE specific settings
    - set debug depending on DEPLOYMENT_TYPE
    - serve /All/ from release download folder for
      non-production environment

--- pybrowser_dev/urls.py
+++ pybrowser_dev/urls.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
+import os
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,3 +13,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^oma/', include('oma.urls')),
 )
+
+if settings.DEPLOYMENT != "PRODUCTION":
+    dwnld_folder = os.path.normpath(os.path.join(os.getenv('DARWIN_BROWSERDATA_PATH', default="./"), '..', 'downloads'))
+    urlpatterns += static(r'All/', document_root=dwnld_folder)
+
