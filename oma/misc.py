85ee9f0215058f1af2df56e76f5a1924c84d97e7
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2015-11-02 08:13:49 +0100

    mayor refactor session
    - added ProteinEntry and Genome as a model class to simplify
      functionality of usage
    
    - added info/fasta view
    
    - new tests on fasta views
    
    - tested py2 / py3.

--- oma/misc.py
+++ oma/misc.py
@@ -1,6 +1,8 @@
 from builtins import range
 import math
-from io import BytesIO
+from functools import wraps
+from io import StringIO
+
 
 def format_sciname(sci, short=False):
     p = set([sci.find(x) for x in ['(','serogroup','serotype','serovar',
@@ -11,17 +13,42 @@ def format_sciname(sci, short=False):
     p = min(p) if len(p)>0 else len(sci)
     return {'species': sci[0:p], 'strain':sci[p:]}
 
+
 def as_fasta(seqs, headers=None):
     if headers is None:
         headers = ['seq{}'.format(i+1) for i in range(len(seqs))]
     if len(seqs) != len(headers):
         raise ValueError('number of headers and sequences does not match')
 
-    buf = BytesIO()
+    buf = StringIO()
     for i, seq in enumerate(seqs):
-        buf.write(('> {}\n'.format(headers[i])).encode('utf-8'))
+        buf.write(u'> {}\n'.format(headers[i]))
         for k in range(0, len(seq), 80):
             buf.write(seq[k:k+80])
-            buf.write(b'\n')
-        buf.write(b'\n')
+            buf.write(u'\n')
+        buf.write(u'\n')
     return buf.getvalue()
+
+
+class LazyProperty(object):
+    """Decorator to evaluate a property only on access.
+
+    Compute the attribute value and caches it in the instance.
+    Python Cookbook (Denis Otkidach) http://stackoverflow.com/users/168352/denis-otkidach
+    This decorator allows you to create a property which can be computed once and
+    accessed many times."""
+    def __init__(self, method, name=None):
+        # record the unbound-method and the name
+        self.method = method
+        self.name = name or method.__name__
+        self.__doc__ = method.__doc__
+
+    def __get__(self, inst, cls):
+        if inst is None:
+            return self
+        # compute, cache and return the instance's attribute value
+        result = self.method(inst)
+        # setattr redefines the instance's attribute so this doesn't get called again
+        setattr(inst, self.name, result)
+        return result
+
