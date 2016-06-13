bb1ae3c3e66310d2955ca077c57b95679693ac54
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2016-06-13 21:59:12 +0200

    code to compute marker genes in the background

--- pybrowser_dev/celery.py
+++ pybrowser_dev/celery.py
@@ -7,7 +7,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pybrowser_dev.settings')
 
 from django.conf import settings  # noqa
 
-app = Celery('proj')
+app = Celery('browser')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
