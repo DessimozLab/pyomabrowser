07e1660bfdc9fc39cfaecaa1689fcdfa48e99056
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2017-07-03 21:23:30 +0200

    generate drf auto docu

--- oma_rest/urls.py
+++ oma_rest/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from . import views
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+
 
 router = DefaultRouter()
 router.register(r'protein', views.ProteinEntryViewSet, base_name='protein')
@@ -16,5 +18,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^pairs/(?P<genome_id1>\w+)/(?P<genome_id2>\w+)/$',
         views.PairwiseRelationAPIView.as_view(), name='pairs'),
-    url(r'^schema/$', get_schema_view(title="OMA REST API"))
+    url(r'^schema/$', get_schema_view(title="OMA REST API")),
+    url(r'^docs', include_docs_urls(title='REST API for the OMA Browser'))
 ]
