5b676c1daaa53fb0ee78cedf32ec1531891ad731
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2017-07-28 00:11:40 +0200

    fixes wrong default tab in use-cases help page

--- functional_tests/test_help_pages_valid.py
+++ functional_tests/test_help_pages_valid.py
@@ -17,3 +17,16 @@ class HelpPagesTest(FunctionalTest):
         # It contains a paragraph with 'OMA in a nutshell' as header
         nutshell_div = self.browser.find_element_by_id('nut')
         self.assertIn('OMA in a nutshell', nutshell_div.text)
+
+        # Now, we are interested in some of the use cases of orthology.
+        # Tom clicks on the "typical uses" menu.
+        self.browser.find_element_by_link_text('Help').click()
+        self.browser.find_element_by_link_text('Typical uses').click()
+        # we check the active tab corresponds to the active tab and that only one tab is active
+        container = self.browser.find_element_by_class_name('container')
+        active_tab_id = [el.get_attribute('id') for el in container.find_elements_by_class_name('active') if el.tag_name == "div"]
+        active_li = [el for el in container.find_elements_by_class_name('active') if el.tag_name == 'li']
+        self.assertEqual(1, len(active_li))
+        self.assertEqual(1, len(active_tab_id))
+        self.assertTrue(active_li[0].find_element_by_tag_name('a').get_attribute('href').endswith(active_tab_id[0]))
+
