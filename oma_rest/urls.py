e0a963705caf63dbd23a6d78887b91186f55635c
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2017-08-30 11:14:11 +0200

    hook for description of documenation page

--- oma_rest/urls.py
+++ oma_rest/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
-
+from django.template.loader import render_to_string
 
 router = DefaultRouter()
 router.register(r'protein', views.ProteinEntryViewSet, base_name='protein')
@@ -15,13 +15,14 @@ router.register(r'hog', views.HOGsViewSet, base_name='hog')
 router.register(r'taxonomy', views.TaxonomyViewSet, base_name='taxonomy')
 
 
-
+# create docu-description from template
+desc = render_to_string("oma_rest/documentation_description.html", {})
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^pairs/(?P<genome_id1>\w+)/(?P<genome_id2>\w+)/$',
         views.PairwiseRelationAPIView.as_view(), name='pairs'),
     url(r'^schema/$', get_schema_view(title="OMA REST API")),
-    url(r'^docs', include_docs_urls(title='REST API for the OMA Browser')),
+    url(r'^docs', include_docs_urls(title='REST API for the OMA Browser', description=desc)),
 ]
 
 
