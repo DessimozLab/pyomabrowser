a760dc202c12afb2055d848e78dadbfada493a68
ctrain <clement.train@gmail.com>
2017-09-25 18:11:44 +0200

    filter-taxon: version pre-alpha phylo.io integration

--- oma/static/js/bootstrap-table-filtertaxon.js
+++ oma/static/js/bootstrap-table-filtertaxon.js
@@ -16,31 +16,31 @@
 
     var sprintf = $.fn.bootstrapTable.utils.sprintf;
 
-    var build_tax_to_species_converter = function(url_tree, list_taxons){
+    var build_tax_to_species_converter = function (url_tree, list_taxons) {
 
-        var converter = {};
+        var converter = {'custom': []};
 
         $.ajaxSetup({
-    async: false
-});
+            async: false
+        });
 
-        $.getJSON(url_tree, function(json) {
+        $.getJSON(url_tree, function (json) {
             var tree_vis = tnt.tree().data(json);
 
             var root = tree_vis.root();
 
-            for (var e in list_taxons){
+            for (var e in list_taxons) {
 
                 var node = root.find_node_by_name(list_taxons[e]);
 
-                if (node){
+                if (node) {
                     var leaves = node.get_all_leaves();
 
                     if (leaves) {
 
                         converter[list_taxons[e]] = [];
 
-                        for (var i in leaves){
+                        for (var i in leaves) {
 
                             converter[list_taxons[e]].push(leaves[i].node_name());
 
@@ -55,21 +55,20 @@
         });
 
         $.ajaxSetup({
-    async: true
-});
+            async: true
+        });
 
 
         return converter
 
     };
 
-    /*
 
     var showAvdSearch = function (pColumns, searchTitle, searchText, that) {
 
         if (!$("#avdSearchModal" + "_" + that.options.idTable).hasClass("modal")) {
             var vModal = sprintf("<div id=\"avdSearchModal%s\"  class=\"modal fade\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"mySmallModalLabel\" aria-hidden=\"true\">", "_" + that.options.idTable);
-            vModal += "<div class=\"modal-dialog modal-xs\">";
+            vModal += "<div class=\"modal-dialog modal-lg\">";
             vModal += " <div class=\"modal-content\">";
             vModal += "  <div class=\"modal-header\">";
             vModal += "   <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\" >&times;</button>";
@@ -85,31 +84,14 @@
 
             $("body").append($(vModal));
 
-            var vFormAvd = createFormAvd(pColumns, searchText, that),
-                timeoutId = 0;
-            ;
+            var phyloContent = add_phyloioDOM(), jmodal = $('#avdSearchModalContent' + "_" + that.options.idTable);
 
-            $('#avdSearchModalContent' + "_" + that.options.idTable).append(vFormAvd.join(''));
+            jmodal.append(phyloContent.join(''));
 
-            $('#' + that.options.idForm).off('keyup blur', 'input').on('keyup blur', 'input', function (event) {
-                clearTimeout(timeoutId);
-                timeoutId = setTimeout(function () {
-                    that.onColumntaxonFilter(event);
-                }, that.options.searchTimeOut);
-            });
+            init_phyloIo(that);
 
-            $('#test_button').off('click').on('click', function (event) {
+            jmodal.append(sprintf('<button type="button" id="btnCloseAvd%s" class="btn btn-default pull-right" >%s</button>', "_" + that.options.idTable, searchText));
 
-                //test_searchmode = true;
-
-                //console.log(test_request);
-                //console.log($(event.currentTarget)[0].id);
-
-                that.onColumntaxonFilter(event);
-
-                //test_searchmode = false;
-
-            });
 
             $("#btnCloseAvd" + "_" + that.options.idTable).click(function () {
                 $("#avdSearchModal" + "_" + that.options.idTable).modal('hide');
@@ -121,36 +103,134 @@
         }
     };
 
-    var createFormAvd = function (pColumns, searchText, that) {
+    var init_phyloIo = function (that) {
 
-        var htmlForm = [];
+        var numberOfSelectedGenome = 0;
+        var maxGenome = 1000000; //number max of selected genomes
+        var hashGenome = {};
+        var needUpdate = false;//Toolbox need to be update ?
+        var arrayIdSelectedGenome = [];
+        // var insearch=false;
+        var contectMenu = false;
+        var contextNode = null;
+        // var tooManyGenomesInBranch=false;
 
-        htmlForm.push('<button class="btn btn-default center-block" type="button" id="test_button" aria-label="advanced search" title="cc">Filter w/ tet + homo</button> <br>');
+        var mouse = {x: 0, y: 0};
+        document.addEventListener('mousemove', function (e) {
+            mouse.x = e.clientX || e.pageX;
+            mouse.y = e.clientY || e.pageY
+        }, false);
 
-        htmlForm.push(sprintf('<form class="form-horizontal" id="%s" action="%s" >', that.options.idForm, that.options.actionForm));
-        for (var i in pColumns) {
-            var vObjCol = pColumns[i];
-            if (!vObjCol.checkbox && vObjCol.visible && vObjCol.searchable) {
-                htmlForm.push('<div class="form-group">');
-                htmlForm.push(sprintf('<label class="col-sm-4 control-label">%s</label>', vObjCol.title));
-                htmlForm.push('<div class="col-sm-6">');
-                htmlForm.push(sprintf('<input type="text" class="form-control input-md" name="%s" placeholder="%s" id="%s">', vObjCol.field, vObjCol.title, vObjCol.field));
-                htmlForm.push('</div>');
-                htmlForm.push('</div>');
+        var viewerHeight = 480;  //height of the browser window
+        var svg_cont = $('#svgg');
+        svg_cont.css('height', viewerHeight); // svgheight = window size - navheight - (some of margin + padding)
+
+        svg_cont.scroll(function () {
+                if ($('.tooltip')) {
+                    $('.tooltip').remove();
+                    contectMenu = false;
+                    contextNode = null;
+                }
+            });
+
+        //Recursive tree visit with function for the actual node and for select children
+        function visit(parent, visitFn, childrenFn) {
+            if (!parent) return;
+            visitFn(parent);
+            var children = childrenFn(parent);
+            if (children) {
+                var count = children.length;
+                for (var i = 0; i < count; i++) {
+                    visit(children[i], visitFn, childrenFn);
+                }
             }
         }
 
-        htmlForm.push('<div class="form-group">');
-        htmlForm.push('<div class="col-sm-offset-9 col-sm-3">');
-        htmlForm.push(sprintf('<button type="button" id="btnCloseAvd%s" class="btn btn-default" >%s</button>', "_" + that.options.idTable, searchText));
-        htmlForm.push('</div>');
-        htmlForm.push('</div>');
-        htmlForm.push('</form>');
+        function OpenInNewTab(url) {
+            var win = window.open(url, '_blank');
+            win.focus();
+        }
 
-        return htmlForm;
-    };
+        function build_dicts(root) {
+            visit(root, function (d) {
+                    if (!(d.children || d._children)) {
+                        hashGenome[d.name] = d.id
+                    }
+                },
+                function (d) {
+                    if (d.children && d.children.length > 0) {
+                        return d.children;
+                    } else if (d._children && d._children.length > 0) {
+                        return d._children;
+                    }
+                });
+        };
 
-    */
+        // //load the data from the Json file
+        $.ajax({
+            url: that.options.urlSpecieTree,
+            success: function (newick) {
+                var additionalNodeFunctions = {
+                    "selectForExport": [
+                        function (exportList) {
+                            arrayIdSelectedGenome = exportList;
+                            updateInfo(arrayIdSelectedGenome, tree1.root)
+                        }
+                    ]
+                };
+
+                var treecomp = TreeCompare().init({
+                    maxNumGenome: maxGenome,
+                    nodeFunc: additionalNodeFunctions
+                });
+                var tree1 = treecomp.addTree(newick, undefined);
+                treecomp.changeCanvasSettings({
+                    autoCollapse: 0, //tree1.data.autoCollapseDepth,
+                    enableScale: false
+                });
+                treecomp.viewTree(tree1.name, "container_phylo");
+                needUpdate = true;
+
+                build_dicts(tree1.root);
+                
+            },
+            dataType: "text"
+        });
+
+    }
+
+    var add_phyloioDOM = function () {
+
+        var htmlPhylo = [];
+
+        htmlPhylo.push('<div class="container-fluid">');
+
+        htmlPhylo.push('<div class="row">');
+
+        htmlPhylo.push('<div class="col-md-9 col-sm-12">');
+
+        htmlPhylo.push('<div class="" id="container_phylo" style="background-color:#edf; width: 100%; height: 480px">');
+        htmlPhylo.push('</div>');
+
+        htmlPhylo.push('</div>');
+
+
+        htmlPhylo.push('<div class="col-md-3 col-sm-12">');
+        htmlPhylo.push('<h4 id="textSel"> Selected genomes</h4>');
+        htmlPhylo.push('<ul class="list-group" id="ul_selection">');
+
+        htmlPhylo.push('</ul>');
+        htmlPhylo.push('</div>');
+
+        htmlPhylo.push('</div>');
+
+        htmlPhylo.push('</div>');
+
+
+        return htmlPhylo;
+
+    }
+
 
     $.extend($.fn.bootstrapTable.defaults, {
         taxonFilter: false,
@@ -173,10 +253,10 @@
 
     $.extend($.fn.bootstrapTable.locales, {
         formattaxonFilter: function () {
-            return 'Advanced search';
+            return 'Filter the tables with your custom species set';
         },
         formatAdvancedCloseButton: function () {
-            return "Close";
+            return "Apply";
         },
         taxon_to_show: function () {
             return [];
@@ -227,7 +307,10 @@
         html.push(' <li><a  class="li_filtertax"  id="all">All</a></li> ');
 
         for (var tax in this.tax_converter) {
-            html.push(' <li><a  class="li_filtertax" id="' + tax +'">' + tax +'</a></li> ');
+            if (tax !== 'custom') {
+
+                html.push(' <li><a  class="li_filtertax" id="' + tax + '">' + tax + '</a></li> ');
+            }
         }
 
         html.push(' <li class="divider"></li> ');
@@ -241,23 +324,17 @@
         that.$toolbar.find('a[class="li_filtertax"]')
             .off('click').on('click', function (event) {
 
-                if (this.id === 'custom') {
-                    console.log('Custom filtering selection is not yet implemented');
-                    //showAvdSearch(that.columns, that.options.formattaxonFilter(), that.options.formatAdvancedCloseButton(), that);
-                }
-
-                else{
-                        //console.log(event);
-                        that.onColumntaxonFilter(event);
-                }
-
+            if (this.id === 'custom') {
+                showAvdSearch(that.columns, that.options.formattaxonFilter(), that.options.formatAdvancedCloseButton(), that);
+            }
+            else {
+                that.onColumntaxonFilter(event);
+            }
         });
 
-        d3.select("#reset_taxon_filter").on('click', function (){
-
-                that.resetSearch();
-
-            })
+        d3.select("#reset_taxon_filter").on('click', function () {
+            that.resetSearch();
+        })
 
     };
 
@@ -297,9 +374,9 @@
 
             var that = this;
 
-           this.data = $.grep(this.data, function (item, i) {
+            this.data = $.grep(this.data, function (item, i) {
 
-            var lsp = that.tax_converter[that.multi_species_search];
+                var lsp = that.tax_converter[that.multi_species_search];
 
                 for (var sp in lsp) {
 
@@ -320,9 +397,11 @@
         }
 
         if (this.multi_species_search && this.multi_species_search !== 'all' || this.options.searchText) {
-            $('.alert_remove').show();}
+            $('.alert_remove').show();
+        }
         else {
-            $('.alert_remove').hide();}
+            $('.alert_remove').hide();
+        }
 
     };
 
