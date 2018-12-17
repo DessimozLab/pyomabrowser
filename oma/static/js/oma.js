101875d61d0f9fec24fec8e00c1bb3a0b24f7a84
Panu Artimo <panu.artimo@sib.swiss>
2018-12-17 11:16:55 +0100

    change how mobile layout shows cards (WIP)

--- oma/static/js/oma.js
+++ oma/static/js/oma.js
@@ -29,15 +29,31 @@ $( document ).ready(function() {
     sideBarActivation();
 });
 
-$(window).resize(sideBarActivation);
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
-function sideBarActivation() {
 
-
-    if ( $(window).width() < 769) {
+var sideBarActivation = debounce(function() {
+	if ( $(window).width() < 769) {
         $('.oma-mobile-accordion').click(function () {
             $(this).next('.oma-card-content').slideToggle('fast').siblings('.oma-card-content:visible').slideUp('fast');
-            $(this).toggleClass("active").siblings().removeClass("active");
+            $(this).toggleClass("active").siblings().removeClass('active');
         });
     } else {
         // remove click event from the card header
@@ -45,5 +61,7 @@ function sideBarActivation() {
         // show every cards content
         $('.oma-card-content').show();
     }
+}, 250);
 
-}
+window.addEventListener('resize', sideBarActivation);
+
