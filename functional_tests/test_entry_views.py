5fe1fe77a1df114376e3138f268f836bc683eb5d
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2019-08-21 14:11:55 +0200

    use a more stable wheat gene for testing homoeology functionality

--- functional_tests/test_entry_views.py
+++ functional_tests/test_entry_views.py
@@ -8,9 +8,9 @@ class EntryViewPageTest(FunctionalTest):
         # Cecile navigates to OMA Browser page
         self.browser.get(self.server_url)
 
-        # she wants to search for her favorite wheat gene - A0A1D5VN70
+        # she wants to search for her favorite wheat gene - AVLA1_WHEAT
         # she first finds the search field
-        query = 'A0A1D5VN70'
+        query = 'AVLA1_WHEAT'
         inputbox = self.browser.find_element_by_id('inputBrowser')
         # she enters the query gene into it, and presses enter
         inputbox.send_keys('{}\n'.format(query))
