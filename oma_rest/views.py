3116e6b22165a08084f092fe19a3a31749527fee
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2019-11-19 11:32:26 +0100

    add api endpoint for hog_derived_orthologs

--- oma_rest/views.py
+++ oma_rest/views.py
@@ -91,7 +91,7 @@ class ProteinEntryViewSet(ViewSet):
 
     @detail_route()
     def orthologs(self, request, entry_id=None, format=None):
-        """List of all the identified orthologues for a protein.
+        """List of all the identified pairwise orthologs for a protein.
 
         Possible to filter out orthologs by specifying the ?rel_type
         query parameter.
@@ -116,6 +116,19 @@ class ProteinEntryViewSet(ViewSet):
             else:
                 content.append(ortholog)
         serializer = serializers.OrthologsListSerializer(instance=content, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    @detail_route()
+    def hog_derived_orthologs(self, request, entry_id, format=None):
+        """List of the orthologs derived from the hog for a given protein.
+
+        :param entry_id: an unique identifier for a protein - either it
+                         entry number, omaid or its canonical id
+        """
+        p_entry_nr = resolve_protein_from_id_or_raise(entry_id)
+        data = utils.db.get_hog_induced_pairwise_orthologs(p_entry_nr)
+        content = [models.ProteinEntry(utils.db, e) for e in data]
+        serializer = serializers.ProteinEntrySerializer(instance=content, many=True, context={'request': request})
         return Response(serializer.data)
 
     @detail_route()
