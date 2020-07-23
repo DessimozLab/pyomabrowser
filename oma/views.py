9a59760c73c23a51d3af83c82f6dfd49d0cbbd9b
ctrain <clement.train@gmail.com>
2020-07-23 09:43:02 +0200

    profiler: 3

--- oma/views.py
+++ oma/views.py
@@ -1146,8 +1146,10 @@ class HOGSimilarProfile(HOG_Base, TemplateView):
 
         sim_hogs = utils.db.get_families_with_similar_hog_profile(context['hog_fam'])
 
+        t = type(sim_hogs.values()[0])
+
         context.update({
-            'tab': 'similar', 'subtab': 'profile', 'sim_data': sim_hogs})
+            'tab': 'similar', 'subtab': 'profile', 'sim_data': sim_hogs, 'test':t})
         return context
 
 
