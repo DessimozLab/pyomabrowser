8341ef5259179e0fdd28c88bc35f794830161942
ctrain <clement.train@gmail.com>
2020-07-27 21:25:57 +0200

    working profiler

--- oma/static/js/profile.js
+++ oma/static/js/profile.js
@@ -23,18 +23,20 @@
 
             if (!data_profile) return;
 
-            //var data_profile = data_profile.slice(1500,2000);
+            var data_taxon=[{'idx':'0', 'taxon':'taxon1'},{'idx':'500', 'taxon':'taxon2'},{'idx':'1500', 'taxon':'taxon3'},{'idx':'2000', 'taxon':'taxon4'},]
 
             // todo filter bar with no height
+        // todo name on zoon
+
 
             var margin = {top: 20, right: 20, bottom: 20, left: 20},
-            width = 840 - margin.left - margin.right,
+
+                width = container.offsetWidth - margin.left - margin.right,
             height = 120 - margin.top - margin.bottom;
 
             const svg =  d3.select(container).append("svg")
             .attr("viewBox", [0, 0, width, height])
             .call(zoom);
-
 
             var  x = d3.scaleBand()
             .domain(data_profile.map(function(d,i) {return i}))
@@ -47,34 +49,45 @@
 
             var xAxis = g => g
             .attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).tickValues([]))
+            .call(d3.axisBottom(x)
+                .tickValues(data_taxon.map(function(d) {return d.idx}))
+            .tickFormat(function (d) {
+                var result = data_taxon.find(obj => {return obj.idx === d});
+                return result.taxon;
+            }));
+
 
             var yAxis = g => g
             .attr("transform", `translate(${margin.left},0)`)
             .call(d3.axisLeft(y))
             .call(g => g.select(".domain").remove())
 
+            var color_scale = d3.scaleThreshold()
+                .domain(data_taxon.map(function(d) {return d.idx}))
+                .range(d3.schemeCategory10);
 
             svg.append("g")
             .attr("class", "bars")
-            .attr("fill", "steelblue")
+
             .selectAll("rect")
             .data(data_profile)
             .join("rect")
             .attr("x", function(d,i) { return x(i) })
             .attr("y", d => y(d))
-          .attr("height", d => y(0) - y(d))
-          .attr("width", x.bandwidth());
-
-
+                .attr("fill", function(d, i) {return color_scale(i)})
+            .attr("height", d => y(0) - y(d))
+            .attr("width", x.bandwidth());
 
             svg.append("g")
             .attr("class", "x-axis")
-            .call(xAxis);
-
+            .call(xAxis)
+        .selectAll("text")
+    .attr("dy", ".35em")
+                .attr("font-weight", 700)
+    .attr("transform", 'translate(+16,-0)')
+            .style("text-anchor", "start");
 
             svg.node();
-
 
             function zoom(svg) {
 
@@ -90,19 +103,22 @@
 
 
   function zoomed() {
+
+      console.log(d3.event.transform.k)
     x.range([margin.left, width - margin.right].map(d => d3.event.transform.applyX(d)));
     svg.selectAll(".bars rect").attr("x", function(d,i) { return x(i) }).attr("width", x.bandwidth());
-    svg.selectAll(".x-axis").call(xAxis);
+
+    if (d3.event.transform.k > 25){
+        svg.selectAll(".x-axis").call(xAxis)
+    }
+    else {
+        svg.selectAll(".x-axis").call(xAxis)
+    }
+
+    ;
   }
 }
 
-
-
-
-
     };
-
-
-
 
 })(this.profile={});
