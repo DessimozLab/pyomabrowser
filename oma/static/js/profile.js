be20414e7bad8411eb0aed1f3d475b7968d0f334
ctrain <clement.train@gmail.com>
2020-10-15 10:24:24 +0200

    profile in table

--- oma/static/js/profile.js
+++ oma/static/js/profile.js
@@ -2,17 +2,454 @@
     "use strict";
 
 
-
     var margin = {top: 20, right: 20, bottom: 20, left: 20};
-    var margin2 = {top: 5, right: 20, bottom: 15, left: 20};
-    var attr_name = 'id';
-    var class_of_entries = ".profile_vis";
-    var class_ref = ".profile_vis_ref";
-    var class_overview = ".overview";
-    var minimap_factor = null;
+    var height = 120 - margin.top - margin.bottom;
 
 
-        exports.visualize_header = function (id, ref_profile_data, taxon, species) {
+
+
+
+     exports.visualize = function (container, data_profile, taxon , species){
+
+         if (!data_profile) return;
+
+        var filter_data = [];
+         var width = container.offsetWidth - margin.left - margin.right;
+
+        $.each(data_profile, function (idx, val) {
+
+            if (val === 1) {
+                filter_data.push(idx);
+            }
+
+        });
+
+
+        var _root = [];
+        var _200 = [];
+        var _50 = [];
+
+        $.each(taxon, function (level, ltax) {
+
+            if (level == 'root') {
+
+                $.each(ltax, function (name, idx) {
+                    _root.push({'idx': idx[0], 'taxon': name});
+                })
+            }
+
+            else if (level == '50') {
+
+                $.each(ltax, function (name, idx) {
+                    _50.push({'idx': idx[0], 'taxon': name});
+                })
+            }
+
+            else if (level == '200') {
+
+                $.each(ltax, function (name, idx) {
+                    _200.push({'idx': idx[0], 'taxon': name});
+                })
+            }
+
+        });
+
+        var sp_array = []
+        $.each(species, function (idx, name) {
+            sp_array.push({'idx': idx, 'name': name});
+        })
+
+
+
+        var data_taxon = function (zoom_level) {
+
+
+            if (zoom_level > 20) {
+                return _50
+            }
+            else if (zoom_level > 5) {
+                return _200
+            }
+            else {
+                return _root
+            }
+        }
+
+        var zoom = d3.zoom()
+        .scaleExtent([1, Infinity])
+        .translateExtent([[0, 0], [width, height]])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", zoomed);
+
+        function zoomed() {
+  if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+
+
+
+        var e = d3.event.transform;
+
+              x.range([margin.left, width - margin.right]
+                    .map(d => e.applyX(d)));
+
+                svg.selectAll(".bars rect")
+                    .attr("x", function (d) {
+                        return x(d)
+                    })
+                    .attr("width", x.bandwidth());
+
+                svg.selectAll(".x-axis").call(xAxis).selectAll("text")
+                    .style("text-anchor", "start")
+                    .style("font-style",function (d) {return e.k > 5 ? "italic" : "normal"})
+                    .style("font-weight",function (d) {return e.k <= 5 ? "bold" : "plain"})
+                    .style("text-anchor", "start")
+
+}
+
+
+
+        var svg = d3.select(container).append("svg")
+            .attr("viewBox", [0, 0, width, height])
+            .attr("class" ,"zoom")
+            .call(zoom)
+            .on("mousedown.zoom", null)
+
+        var x = d3.scaleBand()
+            .domain(data_profile.map(function (d, i) {
+                return i
+            }))
+            .range([margin.left, width - margin.right])
+            .padding(0.1)
+
+
+        var y = d3.scaleLinear()
+            .domain([0, d3.max(data_profile, d => d)]).nice()
+            .range([height - margin.bottom, margin.top])
+
+        var xAxis = g => g
+            .attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x)
+                .tickValues(data_taxon(d3.zoomTransform(svg.node()).k).map(function (d) {
+                    return d.idx
+                }))
+                .tickFormat(function (d) {
+                    var result = data_taxon(d3.zoomTransform(svg.node()).k).find(obj => {
+                        return obj.idx === d
+                    });
+                    return result.taxon;
+                }));
+
+
+        var yAxis = g => g
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(d3.axisLeft(y))
+            .call(g => g.select(".domain").remove())
+
+        var color_scale = d3.scaleLinear()
+            .domain(data_taxon(d3.zoomTransform(svg.node()).k)
+                .map(function (d) {
+                    return d.idx
+                }))
+            .range(["green", "blue", "red"]);
+
+        // Define the div for the tooltip
+        const div = d3
+            .select('body')
+            .append('div')
+            .attr('class', 'tooltip')
+            .style('opacity', 0);
+
+        svg.append("g")
+            .attr("class", "bars")
+            .selectAll("rect")
+            .data(filter_data)
+            .join("rect")
+            .attr("x", function (d) {
+                return x(d)
+            })
+            .attr("y", d => y(1))
+            .attr("fill", function (d) {
+                return color_scale(d)
+            })
+            .attr("height", d => y(0) - y(1))
+            .attr("width", x.bandwidth())
+            .on('mouseover',
+                function (d) {
+
+
+                    div.transition().duration(200).style('opacity', 0.9);
+                    div.html(sp_array.find(obj => {
+                        return obj.idx === d
+                    }).name)
+                        .style('left', d3.event.pageX + 'px')
+                        .style('top', d3.event.pageY - 28 + 'px');
+                })
+            .on('mouseout', () => {
+                div
+                    .transition()
+                    .duration(500)
+                    .style('opacity', 0);
+            });
+
+        svg.append("g")
+            .attr("class", "x-axis")
+            .call(xAxis)
+            .selectAll("text")
+            .style("font-style", function (d) {
+                return d3.zoomTransform(svg.node()).k > 5 ? "italic" : "normal"
+            })
+            .style("font-weight", function (d) {
+                return d3.zoomTransform(svg.node()).k <= 5 ? "bold" : "plain"
+            })
+            .style("text-anchor", "start")
+
+
+        svg.node();
+
+
+        return {'svg': svg, 'x': x, 'xAxis': xAxis, 'color_scale': color_scale, 'zoom':zoom};
+
+     };
+
+
+     exports.bind_zoom = function (ref, container_overview, profiler_stack){
+
+         var svg = ref['svg']
+         var x = ref['x']
+
+         var width = container_overview.offsetWidth - margin.left - margin.right;
+
+
+        var brush_xtent = [[margin.left, margin.top], [width - margin.right, height - margin.bottom]]
+
+        var brush = d3.brushX()
+            .extent(brush_xtent)
+            .on("brush end", function () {
+
+
+                var s = d3.event.selection
+
+                d3.select(this).selectAll(".handle--custom").attr("transform", function (d, i) {
+                        return "translate(" + s[i] + "," + - margin.bottom + ")";
+                    });
+
+                brushed(s, profiler_stack[0])
+            });
+
+        var brushResizePath = function (d) {
+
+
+            var e = +(d.type === "e"),
+                x = e ? 1 : -1,
+                y = height -margin.bottom -margin.bottom ;
+            return "M" + (.5 * x) + "," + y + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) + "V" + (2 * y - 6) + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "Z" + "M" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) + "M" + (4.5 * x) + "," + (y + 8) + "V" + (2 * y - 8);
+        };
+
+        var gg = svg.append("g")
+            .attr("class", "brush").call(brush)
+            .call(brush.move, x.range())
+
+            var gh = gg.selectAll(".handle--custom")
+            .data([{type: "w"}, {type: "e"}]).enter().append("path")
+            .attr("class", "handle--custom")
+            .attr("stroke", "#000")
+            .attr("cursor", "ew-resize")
+            .attr("d", brushResizePath);
+
+
+        svg.selectAll(".handle--custom").attr("transform", function (d, i) {
+                        return "translate(" + brush_xtent[i][0]  + "," + - margin.bottom  + ")";
+                    });
+
+        function brushed(s, ref_prof) {
+
+
+
+            if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+            var s = d3.event.selection
+
+            $.each(profiler_stack, function (idx, chart_e) {
+
+
+                chart_e.svg.call(chart_e.zoom.transform, d3.zoomIdentity.scale( (width  / (s[1] - s[0]))).translate(-s[0], 0));
+
+                chart_e.svg.on("wheel.zoom", null);
+
+            })
+
+        }
+     };
+
+
+     exports.run = function (data_profile, data_profile_ref, data_taxon, data_species, container_reference_selector, container_profile_selector  ){
+
+        var profiler_stack = [];
+        var ref;
+
+        var container_ref = document.querySelector(container_reference_selector);
+        ref = exports.visualize(container_ref, data_profile_ref["profile"], data_taxon , data_species )
+
+        $.each(data_profile, function (each, value) {
+
+            var td_profile = document.querySelector('[data-uniqueid="' + value.id+'"]').querySelector(container_profile_selector);
+
+            profiler_stack.push(exports.visualize(td_profile, value.profile, data_taxon , data_species ))
+
+        })
+
+        //exports.bind_zoom(ref,container_ref, profiler_stack)
+
+
+         //!\\ see how resize window perform now with table //!\\
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    exports.visualize_headerxxxxxxxxx = function (id_container, data_profile, taxon , species ){
+
+         var container_overview = document.getElementById(id_container);
+
+         var width = container_overview.offsetWidth - margin.left - margin.right;
+
+
+        var _root = [];
+        var _200 = [];
+        var _50 = [];
+
+        $.each(taxon, function (level, ltax) {
+
+            if (level == 'root') {
+
+                $.each(ltax, function (name, idx) {
+                    _root.push({'idx': idx[0], 'taxon': name});
+                })
+            }
+
+            else if (level == '50') {
+
+                $.each(ltax, function (name, idx) {
+                    _50.push({'idx': idx[0], 'taxon': name});
+                })
+            }
+
+            else if (level == '200') {
+
+                $.each(ltax, function (name, idx) {
+                    _200.push({'idx': idx[0], 'taxon': name});
+                })
+            }
+
+        });
+
+        var sp_array = []
+        $.each(species, function (idx, name) {
+            sp_array.push({'idx': idx, 'name': name});
+        })
+
+        var data_taxon = _root
+
+
+        var filter_data = []
+
+        $.each(data_profile, function (idx, val) {
+
+            if (val === 1) {
+                filter_data.push(idx);
+            }
+
+        });
+
+        //
+        var svg = d3.select(container_overview).append("svg")
+            .attr("viewBox", [0, 0, width, height])
+
+        //
+        var x = d3.scaleBand()
+            .domain(data_profile.map(function (d, i) {
+                return i
+            }))
+            .range([margin.left, width - margin.right])
+            .padding(0.1)
+
+        //
+        var y = d3.scaleLinear()
+            .domain([0, 1])
+            .range([height - margin.bottom, margin.top])
+
+        //
+        var xAxis = g => g
+            .attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x)
+                .tickValues(data_taxon.map(function (d) {
+                    return d.idx
+                }))
+                .tickFormat(function (d) {
+                    var result = data_taxon.find(obj => {
+                        return obj.idx === d
+                    });
+                    return result.taxon;
+                }));
+
+        var yAxis = g => g
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(d3.axisLeft(y))
+            .call(g => g.select(".domain").remove())
+
+        //
+        var color_scale = d3.scaleLinear()
+            .domain(data_taxon.map(function (d) {
+                return d.idx
+            }))
+            .range(["green", "blue", "red"]);
+
+
+        svg.append("g")
+            .attr("class", "bars")
+            .selectAll("rect")
+            .data(filter_data)
+            .join("rect")
+            .attr("x", function (d) {
+                return x(d)
+            })
+            .attr("y", d => y(1))
+            .attr("fill", function (d) {
+                return color_scale(d)
+            })
+            .attr("height", d => y(0) - y(1))
+            .attr("width", x.bandwidth())
+
+        svg.append("g")
+            .attr("class", "x-axis")
+            .call(xAxis)
+            .selectAll("text")
+            .style("font", "8px times")
+            .style("font-weight", "bold")
+            .style("text-anchor", "start")
+
+        svg.node();
+
+
+
+            return [svg, x]
+     }
+
+
+    exports.visualize_headerxxxxxxxx = function (id, ref_profile_data, taxon, species) {
 
         var container_overview = document.getElementById(id);
 
@@ -146,11 +583,12 @@
     }
 
 
-    exports.visualize = function(container, data, taxon_data, species_data){
+    exports.visualizexxxxxxx = function(container, data, taxon_data, species_data){
         exports.run_profile_vis(container, data, taxon_data, species_data, margin, 200, 50);
     };
 
-    exports.run_profile_vis = function (container, data_profile, taxon, species, margin, width, height) {
+
+    exports.run_profile_visxxxxxxx = function (container, data_profile, taxon, species, margin, width, height) {
 
         if (!data_profile) return;
 
@@ -314,6 +752,8 @@
             .on('mouseover',
                 function (d) {
 
+
+
                     div.transition().duration(200).style('opacity', 0.9);
                     div.html(sp_array.find(obj => {
                         return obj.idx === d
@@ -350,24 +790,7 @@
     };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    exports.visualize_all = function (profile_data, taxon_data, species_data, ref_profile_data) {
+    exports.visualize_allxxxxxxx = function (profile_data, taxon_data, species_data, ref_profile_data) {
 
         var _root = [];
         var _200 = [];
@@ -441,8 +864,7 @@
     };
 
 
-
-    exports.bind_brush_and_zoom = function (minimap, array_chart) {
+    exports.bind_brush_and_zoomxxxxx = function (minimap, array_chart) {
 
 
 
@@ -511,6 +933,18 @@
 
 
     }
+
+/*
+
+    var margin = {top: 20, right: 20, bottom: 20, left: 20};
+    var margin2 = {top: 5, right: 20, bottom: 15, left: 20};
+    var attr_name = 'id';
+    var class_of_entries = ".profile_vis";
+    var class_ref = ".profile_vis_ref";
+    var class_overview = ".overview";
+    var minimap_factor = null;
+
+*/
 
 
 
