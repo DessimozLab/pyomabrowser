db084b09d82e62e33efa9d2433f12ceac62167bd
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-02-04 09:00:37 +0100

    modify navbar based on installed apps

--- oma/context_processors.py
+++ oma/context_processors.py
@@ -11,7 +11,9 @@ def oma_instance(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
     res = {
         "oma_instance_name": settings.OMA_INSTANCE_NAME,
-        "oma_academy": "academy" in settings.INSTALLED_APPS,
+        "oma_academy_enabled": "academy" in settings.INSTALLED_APPS,
+        "fastmap_enabled": "fastmap" in settings.INSTALLED_APPS,
+        "export_enabled": "export" in settings.INSTALLED_APPS,
     }
     try:
         res["google_tracker_code"] = settings.GOOGLE_ANALYTICS["google_analytics_id"]
