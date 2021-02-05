138d8e606540581c8dc28712661bc11e6390c0f0
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-02-05 17:27:43 +0100

    try various places to load genomes.json from

--- oma/utils.py
+++ oma/utils.py
@@ -2,6 +2,8 @@ import pyoma.browser.db
 import pyoma.browser.models
 import django.conf
 import functools
+import os
+import json
 
 
 db = pyoma.browser.db.Database(django.conf.settings.HDF5DB['PATH'])
@@ -15,3 +17,16 @@ HOG = functools.partial(pyoma.browser.models.HOG, db)
 Genome = functools.partial(pyoma.browser.models.Genome, db)
 PairwiseRelation = functools.partial(pyoma.browser.models.PairwiseRelation, db)
 GeneOntologyAnnotation = functools.partial(pyoma.browser.models.GeneOntologyAnnotation, db)
+
+
+def load_genomes_json_file():
+    for path in (os.getenv('DARWIN_BROWSERDATA_PATH'),
+                 os.path.join(os.getenv('DARWIN_BROWSERDATA_PATH'),'..', 'downloads'),
+                 os.getenv('DARWIN_BROWSERDATA_PATH'),
+                 ""):
+        fn = os.path.join(path, "genomes.json")
+        if os.path.exists(fn):
+            with open(fn, 'rb', encoding='utf8') as fh:
+                data = json.load(fh)
+            return data
+    raise pyoma.browser.db.DBConsistencyError("cannot find \"genomes.json\" file.")
