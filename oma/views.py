9d3c31a3456f561e974590d4883771b76caaaa87
clement.traingmail.com <clement.train@gmail.com>
2021-05-03 11:02:07 +0200

    error import not complete

--- oma/views.py
+++ oma/views.py
@@ -23,7 +23,7 @@ from django.core.mail import EmailMessage
 from django.template import Context
 from django.template.loader import render_to_string, get_template
 from django.contrib.staticfiles.templatetags.staticfiles import static
-
+from django.shortcuts import redirect, resolve_url
 
 
 from collections import OrderedDict, defaultdict
