b2aa517b856a7fff48e03bf6e00d809067114bc8
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2021-09-20 09:34:36 +0200

    working prototype for omamo

--- omamo/urls.py
+++ omamo/urls.py
@@ -2,9 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    #path('search/', views.search, name='search')
-    path('result/<slug:term>/',
-        views.propose_modelorg, name="result"),
+    path('search/', views.search, name='search'),
 ]
 
 
--- omamo/views.py
+++ omamo/views.py
@@ -1,31 +1,37 @@
-import os.path
-
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponseBadRequest
 from oma import utils
-
-import tables
 import pandas
-import os
 import logging
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-def propose_modelorg(request, term):
-    goterm = utils.db.gene_ontology.term_by_id(term)
-    df = pandas.read_csv(settings.OMAMO["CSV"], delimiter="\t", index_col=0)
-    process = df[df["GO ID"] == goterm.id]
-    res = []
-    for rid, row in process.iterrows():
-        row = row.to_dict()
-        row['Human Genes'] = row['Human Genes'].split(",")
-        row["Species Genes"] = row["Species Genes"].split(",")
-        species = utils.Genome(utils.id_mapper['OMA'].genome_from_UniProtCode(row['Species']))
-        row['taxon'] = species.species_and_strain_as_dict
-        row['kingdom'] = species.kingdom
-        res.append(row)
-    context = {'goterm': goterm,
-               'result': res}
-    return render(request, "omamo_result.html", context)
+def search(request):
+    # build a lookup table for the biological process terms for the autocomplete
+    go_lookup = [{'value': "{} - {}".format(val, val.name), 'data': val.id}
+                 for key, val in utils.db.gene_ontology.terms.items() if val.aspect == 1]
+    context = {'go_auto_data': go_lookup}
+    if request.method == 'GET' and 'query' in request.GET:
+        try:
+            goterm = utils.db.gene_ontology.term_by_id(request.GET.get('query'))
+        except ValueError as e:
+            return HttpResponseBadRequest(str(e))
+        df = pandas.read_csv(settings.OMAMO["CSV"], delimiter="\t", index_col=0)
+        process = df[df["GO ID"] == goterm.id]
+        res = []
+        for rid, row in process.iterrows():
+            row = row.to_dict()
+            row['Human Genes'] = row['Human Genes'].split(",")
+            row["Species Genes"] = row["Species Genes"].split(",")
+            species = utils.Genome(utils.id_mapper['OMA'].genome_from_UniProtCode(row['Species']))
+            row['taxon'] = species.species_and_strain_as_dict
+            row['kingdom'] = species.kingdom
+            res.append(row)
+        context['goterm'] = goterm
+        context['result'] = res
+        context['result_tab'] = True
+    else:
+        context['result_tab'] = False
 
+    return render(request, "omamo_search.html", context)
