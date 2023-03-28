9298742a6aa83d88aad567f5e51c6708effa047c
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2023-03-28 09:43:30 +0200

    fixes INSTALLED_APP -> INSTALLED_APPS

--- oma/views.py
+++ oma/views.py
@@ -1970,7 +1970,7 @@ def home(request):
                'nr_groups': utils.db.get_nr_oma_groups(),
                'nr_hogs': utils.db.get_nr_toplevel_hogs(),
                'release': utils.db.get_release_name(),
-               'mailinglist_enabled': 'mailman_subscribe' in settings.INSTALLED_APP,
+               'mailinglist_enabled': 'mailman_subscribe' in settings.INSTALLED_APPS,
                }
     return render(request, template, context)
 
