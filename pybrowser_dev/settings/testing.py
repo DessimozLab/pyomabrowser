8a491aca6cefaca1452ff80aea0c401a9217a780
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2023-04-28 15:00:03 +0200

    cleanup

--- pybrowser_dev/settings/testing.py
+++ pybrowser_dev/settings/testing.py
@@ -1,10 +1,10 @@
 from .prod import *
 
-EMAIL_HOST = "whippee.com"
-EMAIL_PORT = 8025
-EMAIL_HOST_USER = "labfaq@dessimoz.org"
-EMAIL_HOST_PASSWORD = "yZ4J4nsiVwim"
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.getenv("EMAIL_TLS", True)
 
 # for backward compability reasons
 BROKER_URL = CELERY_BROKER_URL
