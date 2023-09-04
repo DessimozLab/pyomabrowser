cf791c04dcc2f7e28840f2b0161a4a9c3494a9aa
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2023-09-04 17:04:07 +0200

    [fix] various issues with the GO enrichment analysis presentation

--- oma/forms.py
+++ oma/forms.py
@@ -70,7 +70,7 @@ class FunctionProjectionUploadFormBase(forms.Form):
 class GoEnrichmentForm(forms.Form):
 
     CHOICES = (('extant', 'Extant genome Gene Ontology Enrichment Analysis'), ('ancestral', 'Ancestral genome Gene Ontology Enrichment Analysis' ),)
-    type = forms.ChoiceField(choices=CHOICES, required=True)
+    type = forms.ChoiceField(choices=CHOICES, required=True, initial="ancestral")
     Foreground = forms.CharField(widget=forms.Textarea(attrs={"rows":"5", 'placeholder': 'Enter your HOG or Gene Ids separeted by space, comma, new line or tabulation', 'required':True}))
     name = forms.CharField(label='Name of Dataset', max_length=64, required=True)
     taxon = forms.CharField(label='Taxonomic Level (Background)', max_length=64, required=False)
