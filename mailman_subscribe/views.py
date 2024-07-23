81a3587c6ab89215b325326a74108baaab531027
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2024-07-23 11:06:38 +0200

    improve logging for mailman form sending

--- mailman_subscribe/views.py
+++ mailman_subscribe/views.py
@@ -17,11 +17,11 @@ class MailmanForm(forms.Form):
 # Create your views here.
 def subscribe(request):
     if request.method == "POST":
-        logger.warning(request.POST)
         form = MailmanForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            logger.error("sending email...")
+            logger.info("sending subscription email with async task...")
             tasks.subscribe_to_mailing_list.delay(data['email'])
             return HttpResponseRedirect(reverse('mailman-thanks'))
+        logger.warning(f"form data is not valid: {form}")
     return HttpResponseRedirect(reverse('home'))
